203f2a1fd06ce81e6b8565d3837471ed94de98b7
Emily Jacobson <emjacobson96@gmail.com>
2024-09-07 22:15:49 -0400

    moved art and words to seprate files

--- hangman/main.py
+++ hangman/main.py
@@ -1,67 +1,20 @@
 import random
-stages = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-word_list = ["aardvark", "baboon", "camel"]
+import words
+import art
+
+word_list = words.words_list
 gameover = False
 display = ""
 guessed_letters = set()
 life = 0
 chosen_word = random.choice(word_list)
-print(chosen_word)
 length = len(chosen_word)
-
+stages = art.stages
+print("Word to guess:\n")
+print("_ " * length)
 
 while display != chosen_word and not gameover:
     print(f'{stages[life]}')
-    print("_ " * length)
     guess = input("Guess a letter: ").lower()
     new_display = ""
 
@@ -71,13 +24,15 @@ while display != chosen_word and not gameover:
     guessed_letters.add(guess)
     for letter in chosen_word:
         if letter == guess or letter in display:
+            print(f"You guessed {guess}")
             new_display += letter
         else:
-            new_display += "_"
+            new_display += "_ "
     display = new_display
     if guess not in chosen_word:
         if life < 6:
             life += 1
+            print(f"{guess} is not a letter")
         else:
             gameover = True
             print(f"Sorry Game Over!\n The correct word was {chosen_word}")
